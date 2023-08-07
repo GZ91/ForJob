96a14ddb3fbfdd7b2ece1a084de0f8a761dd28a6
Зырянов Георгий <zyryanov@sminex.com>
2023-08-07 13:04:45 +0000

    разбил адрес к бд на составляющие для безопасности

--- scripts/reductor-prod-starter.sh
+++ scripts/reductor-prod-starter.sh
@@ -2,7 +2,11 @@
 
 docker_runner() {
     docker run \
-        -e DATABASE_DSN=$DATABASE_DSN \
+        -e DSN_Login=$DSN_Login \
+        -e DSN_Password=$DSN_Password \
+        -e DSN_Address=$DSN_Address \
+        -e DSN_Sslmode=$DSN_Sslmode \
+        -e DSN_BaseName=$DSN_BaseName \
         -e SERVER_ADDRESS=$SERVER_ADDRESS \
         -e LOG_LEVEL=$LOG_LEVEL \
         -e Secret_Key=$Secret_Key \
