56d2351237bedd3999d62a88b5d57b5980eee7ea
zyryanov <zyryanov@sminex.com>
2023-08-29 17:37:04 +0300

    вернул необходимость ручке ping в токене

--- internal/api/http/Middleware/authentification.go
+++ internal/api/http/Middleware/authentification.go
@@ -11,9 +11,6 @@ import (
 
 func (n *NodeUse) Authentication(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.String() == "/ping" {
-			h.ServeHTTP(w, r)
-		}
 		mainLog := []zap.Field{zap.String("URL", r.URL.String()), zap.String("Method", r.Method), zap.String("remote str", r.RemoteAddr)}
 		token := r.Header.Get("Token")
 		ok := token != ""
